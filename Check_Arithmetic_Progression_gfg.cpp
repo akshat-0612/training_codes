2de31a3fb4cd5682b1bbb4c00d2d2cf1ea9aedb6
akshat <akshat9090.fzd@gmail.com>
2022-12-06 11:19:38 +0530

    codes

--- Check_Arithmetic_Progression_gfg.cpp
+++ Check_Arithmetic_Progression_gfg.cpp
@@ -1,38 +1,35 @@
-class Solution{
-    public:
+class Solution
+{
+public:
     bool checkIsAP(int arr[], int n)
     {
         // code here
-        priority_queue<int,vector<int>,greater<int>>min_heap;
-        
-        for(int i=0;i<n;i++){
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+
+        for (int i = 0; i < n; i++)
+        {
             min_heap.push(arr[i]);
         }
-        
-        int a=min_heap.top();
+
+        int a = min_heap.top();
         min_heap.pop();
-        
-        int d=min_heap.top()-a;
-        
-        a=min_heap.top();
+
+        int d = min_heap.top() - a;
+
+        a = min_heap.top();
         min_heap.pop();
-        
-        
-        
-        while(!min_heap.empty()){
-            if((min_heap.top()-a)!=d){
-                
+
+        while (!min_heap.empty())
+        {
+            if ((min_heap.top() - a) != d)
+            {
+
                 return false;
             }
-            
-            a=min_heap.top();    
+
+            a = min_heap.top();
             min_heap.pop();
-            
-            
-            
         }
         return true;
-        
     }
-
 };
