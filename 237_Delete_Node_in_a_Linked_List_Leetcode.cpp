95dc224d1d91f0677917872a7e28807c94b8ad54
akshat <akshat9090.fzd@gmail.com>
2022-12-10 01:47:03 +0530

    linked list

---
+++ 237_Delete_Node_in_a_Linked_List_Leetcode.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+        ListNode* nextnode=node->next;
+        node->val=nextnode->val;
+        node->next=nextnode->next;
+        nextnode->next=NULL;
+        
+    }
+};
